22b030afa85cb5f5f1a6e34af0c0dba076807905
Gregor Adams <greg@pixelass.com>
2016-09-10 17:06:30 +0200

    style(lint): lint build.js
    start linting build script and made it pretty

--- build.js
+++ build.js
@@ -1,14 +1,11 @@
 const fs = require('fs')
 const Log = require('log')
 const browserify = require('browserify')
-const babelify = require('babelify')
 const errorify = require('errorify')
-const uglifyify = require('uglifyify')
+const uglifyify = require('uglifyify') // eslint-disable-line
 const exec = require('child_process').exec
 
 const log = new Log('info')
-
-
 const fileMap = {
   'dist.js': 'abcq'
 }
@@ -17,11 +14,11 @@ const files = Object.keys(fileMap)
 const srcFolder = `${__dirname}/src`
 const buildFolder = `${__dirname}/dist`
 
-
 exec(`rm -rf ${buildFolder}`, (err) => {
   if (err) {
-    throw error
+    throw err
   }
+
   fs.mkdir(buildFolder)
   files.forEach(file => {
     const inFile = `${srcFolder}/${file}`
@@ -38,15 +35,15 @@ exec(`rm -rf ${buildFolder}`, (err) => {
       global: true
     }, 'uglifyify')
 
-    function bundle() {
+    function bundle () {
       b.bundle().pipe(fs.createWriteStream(`${outFile}.js`))
       u.bundle().pipe(fs.createWriteStream(`${outFile}.min.js`))
     }
 
-    b.on('log',  message => log.info(message))
-    b.on('error',  message => log.error(message))
-    u.on('log',  message => log.info(message))
-    u.on('error',  message => log.error(message))
+    b.on('log', message => log.info(message))
+    b.on('error', message => log.error(message))
+    u.on('log', message => log.info(message))
+    u.on('error', message => log.error(message))
     bundle()
   })
 })
